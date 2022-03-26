62f7e1db0e50c96f0d67e67b97d70248d1d4ade2
aliahmedvox <ali.ahmed@voxsmart.com>
2022-03-26 22:45:40 +0000

    DI (KOIN) added

--- AndroidCleanArchitecture/app/src/main/java/com/example/androidcleanarchitecture/MainApplication.kt
+++ AndroidCleanArchitecture/app/src/main/java/com/example/androidcleanarchitecture/MainApplication.kt
@@ -1,11 +1,18 @@
 package com.example.androidcleanarchitecture
 
 import android.app.Application
+import com.example.androidcleanarchitecture.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class MainApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            androidContext(this@MainApplication)
+            modules(appModule)
+        }
     }
 
 }
--- AndroidCleanArchitecture/app/src/main/java/com/example/androidcleanarchitecture/di/MainModule.kt
+++ AndroidCleanArchitecture/app/src/main/java/com/example/androidcleanarchitecture/di/MainModule.kt
@@ -3,14 +3,20 @@ package com.example.androidcleanarchitecture.di
 import android.content.Context
 import com.example.androidcleanarchitecture.network.APIClient
 import com.example.androidcleanarchitecture.network.APIService
+import com.example.androidcleanarchitecture.repository.DataRepository
+import com.example.androidcleanarchitecture.viewmodel.NewsViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val appModule = module {
-
+    single(named("data_repo")) { DataRepository(NetworkModule(androidContext())) }
+    viewModel { NewsViewModel(get(named("data_repo"))) }
 }
 
-class NetworkModule (val context: Context) {
-    fun sourceOfNetwork() : APIService {
+class NetworkModule(val context: Context) {
+    fun sourceOfNetwork(): APIService {
         return APIClient.create()
     }
 }
